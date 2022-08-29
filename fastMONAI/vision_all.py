76bb073387ac5f6fe110d9b656b6352d68beb147
skaliy <145155@stud.hvl.no>
2022-08-29 17:38:04 +0200

    updated README

---
+++ fastMONAI/vision_all.py
@@ -0,0 +1,12 @@
+#imports all of the functions and classes from the fastMONAI library
+
+from .vision_core import *
+from .vision_data import *
+from .vision_augmentation import *
+from .vision_loss import * 
+from .vision_metrics import *
+from .vision_utils import * 
+from .external_data import *
+from .dataset_info import *
+
+from fastai.vision.all import *
