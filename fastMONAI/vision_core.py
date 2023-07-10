7379a215fa44f65c44dc7c28672302f91cf40ab4
skaliy <145155@stud.hvl.no>
2023-07-10 10:05:08 +0200

    Dev: refactorization

--- fastMONAI/vision_core.py
+++ fastMONAI/vision_core.py
@@ -7,63 +7,83 @@ __all__ = ['med_img_reader', 'MetaResolver', 'MedBase', 'MedImage', 'MedMask']
 from .vision_plot import *
 from fastai.data.all import *
 from torchio import ScalarImage, LabelMap, ToCanonical, Resample
-import pickle
-import warnings
 
-# %% ../nbs/01_vision_core.ipynb 4
-def _preprocess(o, reorder, resample):  
-    
+# %% ../nbs/01_vision_core.ipynb 5
+def _preprocess(obj, reorder, resample):
+    """Preprocesses the given object.
+
+    Args:
+        obj: The object to preprocess.
+        reorder: Whether to reorder the object.
+        resample: Whether to resample the object.
+
+    Returns:
+        The preprocessed object and its original size.
+    """
     if reorder:
         transform = ToCanonical()
-        o = transform(o)
-        
-    org_size = o.shape[1:]
-    
-    if resample and not all(np.isclose(o.spacing, resample)):
-            transform = Resample(resample)
-            o = transform(o)
-            
+        obj = transform(obj)
+
+    original_size = obj.shape[1:]
+
+    if resample and not all(np.isclose(obj.spacing, resample)):
+        transform = Resample(resample)
+        obj = transform(obj)
+
     if MedBase.affine_matrix is None:
-        MedBase.affine_matrix = o.affine
-            
-    return o, org_size
+        MedBase.affine_matrix = obj.affine
+
+    return obj, original_size
 
 # %% ../nbs/01_vision_core.ipynb 6
-def _load(fn:(str, Path), dtype=None):
-    '''Private method to load image as either ScalarImage or LabelMap.
+def _load_and_preprocess(file_path, reorder, resample, dtype):
+    """
+    Helper function to load and preprocess an image.
 
     Args:
-        fn : Image path.
-        dtype: Datatype.
+        file_path: Image file path.
+        reorder: Whether to reorder data for canonical (RAS+) orientation.
+        resample: Whether to resample image to different voxel sizes and dimensions.
+        dtype: Desired datatype for output.
 
     Returns:
-        (ScalarImage, LabelMap): An object that contains a 4D tensor and metadata.
-    '''
-
-    if dtype is MedMask: return LabelMap(fn)
-    else: return ScalarImage(fn)
+        tuple: Original image, preprocessed image, and its original size.
+    """
+    org_img = LabelMap(file_path) if dtype is MedMask else ScalarImage(file_path) #_load(file_path, dtype=dtype) 
+    input_img, org_size = _preprocess(org_img, reorder, resample)
+    
+    return org_img, input_img, org_size
 
 # %% ../nbs/01_vision_core.ipynb 7
-def _multi_channel(img_fns:list, reorder:bool, resample:list, dtype):
-    '''Private method to load multisequence data.
+def _multi_channel(image_paths: list, reorder: bool, resample: list, dtype, only_tensor: bool):
+    """
+    Load and preprocess multisequence data.
 
     Args:
-        img_fns: List of image paths s(e.g. T1, T2, T1CE, DWI).
+        image_paths: List of image paths (e.g., T1, T2, T1CE, DWI).
+        reorder: Whether to reorder data for canonical (RAS+) orientation.
+        resample: Whether to resample image to different voxel sizes and dimensions.
+        dtype: Desired datatype for output.
+        only_tensor: Whether to return only image tensor.
 
     Returns:
-        torch.Tensor: A stacked 4D tensor.
-    '''
+        torch.Tensor: A stacked 4D tensor, if `only_tensor` is True.
+        tuple: Original image, preprocessed image, original size, if `only_tensor` is False.
+    """
+    image_data = [_load_and_preprocess(image, reorder, resample, dtype) for image in image_paths]
+    org_img, input_img, org_size = image_data[-1]
 
-    img_list = []
-    for fn in img_fns:
-        o = _load(fn, dtype=dtype)
-        o,_ = _preprocess(o, reorder, resample)
-        img_list.append(o.data[0])            
+    tensor = torch.stack([img.data[0] for _, img, _ in image_data], dim=0)
+    
+    if only_tensor: 
+        dtype(tensor) 
 
-    return dtype(torch.stack(img_list, dim=0))
+    input_img.set_data(tensor)
+    return org_img, input_img, org_size
+
 
 # %% ../nbs/01_vision_core.ipynb 8
-def med_img_reader(fn:(str, Path), # Image path
+def med_img_reader(file_path:(str, Path), # Image path
                    dtype=torch.Tensor, # Datatype (MedImage, MedMask, torch.Tensor)
                    reorder:bool=False, # Whether to reorder the data to be closest to canonical (RAS+) orientation.
                    resample:list=None, # Whether to resample image to different voxel sizes and image dimensions.
@@ -71,16 +91,12 @@ def med_img_reader(fn:(str, Path), # Image path
                   ):
     '''Load and preprocess medical image'''
         
-    if isinstance(fn, str) and ';' in fn:
-        img_fns = fn.split(';')
-        return _multi_channel(img_fns, reorder, resample, dtype=dtype)
+    if isinstance(file_path, str) and ';' in file_path:
+        return _multi_channel(file_path.split(';'), reorder, resample, dtype, only_tensor)
 
-    org_img = _load(fn, dtype=dtype)
-    input_img, org_size = _preprocess(org_img, reorder, resample)
+    org_img, input_img, org_size = _load_and_preprocess(file_path, reorder, resample, dtype)
 
-    if only_tensor: return dtype(input_img.data.type(torch.float))
-
-    return org_img, input_img, org_size
+    return dtype(input_img.data.type(torch.float)) if only_tensor else org_img, input_img, org_size
 
 # %% ../nbs/01_vision_core.ipynb 10
 class MetaResolver(type(torch.Tensor), metaclass=BypassNewMeta):
